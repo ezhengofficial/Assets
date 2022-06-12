f9b3c517cee81f4617dc860c44bf13adfa5a0167
Henry <hcen20@stuy.edu>
2022-06-12 15:11:29 -0400

    Added Pause Menu and Menu

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -40,6 +41,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Debug.Log("Loading Game...");
+        SceneManager.LoadScene("Menu");
     }
 
     public void QuitGame()
