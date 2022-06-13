514ffc50ac225c84c2327ff1ea4a8a34282fe5ff
Henry <hcen20@stuy.edu>
2022-06-12 21:18:58 -0400

    Added Start Menu

--- MainMenu.cs
+++ MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit...");
+    }
 }
